2aea7a28e8dc8defc1d8dd1ffa246251fa9362b2
Brian Fuentes <Brian.Fuentes04@gmail.com>
2024-02-19 21:52:03 -0800

    - Added Nodemon - postSignup in signup controller works and adds user to the database with hashed password

--- controllers/signup.js
+++ controllers/signup.js
@@ -14,32 +14,39 @@ module.exports = {
     try {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-      const user = new User({
+      await User.create({
         userName: req.body.userName,
         email: req.body.email,
         password: hashedPassword,
       });
 
-      User.findOne(
-        { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-        (err, existingUser) => {
-          if (err) {
-            return next(err);
-          }
-          if (existingUser) {
-            req.flash("errors", {
-              msg: "Account with that email address or username already exists.",
-            });
-            return res.redirect("../signup");
-          }
-          user.save((err) => {
-            if (err) {
-              return next(err);
-            }
-            res.redirect("/");
-          });
-        }
-      );
+      res.redirect("/");
+
+    //   User.findOne(
+    //     { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    //     (err, existingUser) => {
+    //       if (err) {
+    //         return next(err);
+    //       }
+    //       if (existingUser) {
+    //         req.flash("errors", {
+    //           msg: "Account with that email address or username already exists.",
+    //         });
+    //         return res.redirect("../signup");
+    //       }
+    //       user.save((err) => {
+    //         if (err) {
+    //           return next(err);
+    //         }
+    //         req.logIn(user, (err) => {
+    //           if (err) {
+    //             return next(err);
+    //           }
+    //           res.redirect("/");
+    //         });
+    //       });
+    //     }
+    //   );
     } catch (err) {}
   },
 };

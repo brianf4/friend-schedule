bf3951c0b44b630e2fe2bd15518b5e27cff5072d
Brian Fuentes <Brian.Fuentes04@gmail.com>
2024-02-19 20:09:27 -0800

    in controller folder moved auth.js code over to signup.js and deleted auth.js

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,9 +1,24 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
 module.exports = {
-    getSignupPage: (req, res) => {
-        try {
-            res.render("pages/signup.ejs");
-        } catch (err) {
-            console.log(err);
-        }
-    },
+  getSignupPage: (req, res) => {
+    try {
+      res.render("pages/signup.ejs");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  postSignup: async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req, body.password);
+
+      const user = new User({
+        userName: req.body.userName,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+    } catch (err) {}
+  },
 };

9cf7c6a4edac402862b1d8a77af1b74f969a8a84
Brian Fuentes <Brian.Fuentes04@gmail.com>
2024-03-18 16:26:23 -0700

    attempting auth

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ router.get('/', mainController.getIndex);
 router.get('/signup', mainController.getSignupPage);
 router.post('/signup', mainController.postSignup);
 router.get('/login', mainController.getLoginPage);
-router.get('/login', mainController.postLogin);
+router.post('/login', mainController.postLogin);
 router.get('/profile', ensureAuth, mainController.getProfile)
 
 
